aa82606046b0b69edc0a6197a8c1f6892f934849
unknown <ashish.rai@tripxl.com>
2024-12-19 13:13:02 +0530

    db connection corrected

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,22 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const mongooseURI = "mongodb://localhost/iNotebook";
+// const mongooseURI = "mongodb://localhost/iNotebook";
+const mongooseURI = "mongodb://127.0.0.1:27017/iNotebook";
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongooseURI , ()=>{
-        console.log("Connected to Database succcesfully");
-    })
-}
+const connectToMongo = async () => {
+  //   mongoose.connect(mongooseURI, () => {
+  //     console.log("Connected to Database succcesfully");
+  //   });
+  try {
+    await mongoose.connect(mongooseURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Adjust the timeout as needed
+    });
+    console.log("Connected to Database successfully");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
 
-module.exports = connectToMongo ;
+module.exports = connectToMongo;
