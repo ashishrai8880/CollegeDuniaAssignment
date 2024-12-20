bb4ee12b800dfef5df8e2bf24986f5e0509ee9b2
Ashish Rai <ashishrai8880@gmail.com>
2024-12-20 10:39:02 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
 // const mongooseURI = "mongodb://localhost/iNotebook";
-// const mongooseURI = "mongodb://127.0.0.1:27017/iNotebook";
-const mongooseURI =
-  "mongodb+srv://ashishrai8880:ashish@cluster0.pqzwb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/iNotebook";
+const mongooseURI = "mongodb://127.0.0.1:27017/iNotebook";
+
 
 const connectToMongo = async () => {
   //   mongoose.connect(mongooseURI, () => {
